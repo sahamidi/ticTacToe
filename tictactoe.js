81bf9a832f67dd7d4f686eaf206d31065670d547
Sal Hamidi <hamidisa87@gmail.com>
2020-08-10 20:19:51 -0400

    Add notes

--- tictactoe.js
+++ tictactoe.js
@@ -1,56 +1,62 @@
 let circleToken = "cell circle"
 let xToken = "cell x"
 let playerWin 
-
-
-
 //Hold cell of game to check for winner and for tie.
 let gameBoard = []
-let arrayOfWinner = []
 
+//Start Game function to clear the board for new game
+function startGame()
+{
+    document.querySelector(".winner").style.display = "none"
+    for (i=0; i<=8; i++)
+        {
+            //Reset Gameboard for new game
+            document.getElementById(`cell-${i}`).setAttribute('class', 'cell')
+            gameBoard = []
+        }
+}
 
-
+/*/Build out gameboard layout with functionality such as click events to apply either 
+either x or circle tokens
+A continuous loop occurs, which will assign to DOM id elements that will be referenced
+later for toke assigning as well as used for identification of the winner
+*/
 for (let i=0; i<=8; i++)
 {
     let cell = document.createElement("div")
     cell.id = "cell-" + i
     document.getElementById("board").appendChild(cell)
     cell.setAttribute("class", "cell")
-    // alternatively
     cell.setAttribute("name", `${i}`)
-    
+    //Begin board with empty set array of gameBoard tokens with neither x or circle
     gameBoard.push("") 
-    //event listeners, are expected to be passed an object function when activatedtakes two argument
-    //take first argument, which is click, the second argument is a function to be called when function happens
-    //contract between you and add event listener add event listener will call your function with that event
-
+    /*
+    Event listener to establish token turn, check for winners, check for ties.  This is 
+    applied to each cell, and identified per div element which makes up the cell.
+    */
     cell.addEventListener('click', function (e)
     {
         handleCellClick(e, i)
     })
 }
 
-
-
-
-//run game until winner is defined.
+//Counter established to alternate turns in the game betweene x and circle
 let count = 1
+//whosTurn defines current turn in the game based counter
 let whosTurn 
 function handleCellClick(event, cellNumber)
 {
-     
      let whosTurn = (count % 2 !== 0) ? xToken:circleToken
-    
-
             cellON = document.getElementById(`cell-${cellNumber}`)
             cellON.setAttribute('class', whosTurn)
             gameBoard[cellNumber]= whosTurn
             findWinner()
             checkTie()
             return count = count + 1
-            
 }
 
+/*Pre-establied patterns of winner options.  These are indexed in the find winner function
+along with gameBOard to see if it is a x or circle token*/
 let winningPatterns = [
     [0,1,2],
     [0,3,6],
@@ -62,6 +68,8 @@ let winningPatterns = [
     [3,4,5],
 ]
 
+/*loop to continuously check for winners by index of winningPattern and gameBoard
+*/
 function findWinner()
 {
         for (i=0; i < winningPatterns.length; i++)
@@ -77,12 +85,14 @@ function findWinner()
                         {
                             document.getElementById("winner").style.display = 'block'; 
                             document.getElementById("announceWinner").innerText = "X is the Winner. click to Replay"
+                            //Reset game and gameboard
                             document.getElementById('replay').addEventListener('click', startGame)
                         }   
                         else if (gameBoard[winningPatterns[i][0]] == "cell circle")
                         {
                             document.getElementById("winner").style.display = 'block'; 
                             document.getElementById("announceWinner").innerText = "Circle is the Winner. click to Replay"
+                            //Reset game and gameboard
                             document.getElementById('replay').addEventListener('click', startGame)
                         }     
                     }
@@ -90,13 +100,12 @@ function findWinner()
         }
 }      
 
-
+//Checking for tie each round
 function checkTie()
     {
         let thisCounter = 0 
         for (i=0; i <= gameBoard.length; i++)
-        {
-            
+        {            
             if (gameBoard[i] == "cell circle" || gameBoard[i] == "cell x")
             {
                 thisCounter = thisCounter+1
@@ -104,20 +113,9 @@ function checkTie()
                 {
                     document.getElementById("winner").style.display = 'block'; 
                     document.getElementById("announceWinner").innerText = "You tied. click to Replay"
+                    //Reset game and gamboard
                     document.getElementById('replay').addEventListener('click', startGame)
                 }    
             }
-
         }
-
     }
-
-function startGame()
-{
-    document.querySelector(".winner").style.display = "none"
-    for (i=0; i<=8; i++)
-        {
-            document.getElementById(`cell-${i}`).setAttribute('class', 'cell')
-            gameBoard = []
-        }
-}
